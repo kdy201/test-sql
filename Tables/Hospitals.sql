abe28813e69688c934c627face93e6bb82cdd9ae
Kayla Yasmine <kdy201@exeter.ac.uk>
2025-12-07 22:22:15 +0000

    Add hospitals.sql schema file

--- Tables/Hospitals.sql
+++ Tables/Hospitals.sql
@@ -1,5 +1,17 @@
 CREATE DATABASE IF NOT EXISTS hospitals_db;
 USE hospitals_db;
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS Hospitals;
+DROP TABLE IF EXISTS Doctors;
+DROP TABLE IF EXISTS Patients;
+DROP TABLE IF EXISTS Appointments;
+DROP TABLE IF EXISTS Lab_Results;
+DROP TABLE IF EXISTS Medications;
+DROP TABLE IF EXISTS Prescriptions;
+DROP TABLE IF EXISTS Specialists;
+DROP TABLE IF EXISTS Diseases;
+DROP TABLE IF EXISTS Disease_Medication;
+
 
 SHOW VARIABLES LIKE 'secure_file_priv';
 
@@ -41,7 +53,7 @@ appointment_time TIME,
 appointment_type VARCHAR(50),
 FOREIGN KEY (patient_id) REFERENCES Patients(patient_id),
 FOREIGN KEY (doctor_id) REFERENCES Doctors(doctor_id),
-FOREIGN KEY (hospital_id) REFERENCES Hospitals(hospitals_id)
+FOREIGN KEY (hospital_id) REFERENCES Hospitals(hospital_id)
 );
 
 CREATE TABLE Lab_Results (
@@ -98,70 +110,70 @@ FOREIGN KEY (disease_id) REFERENCES Diseases(disease_id)
 );
 
 
-LOAD DATA INFILE '/path/to/Hospitals.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Hospitals.csv'
 INTO TABLE Hospitals
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Doctors.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Doctors.csv'
 INTO TABLE Doctors
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Patients.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Patients.csv'
 INTO TABLE Patients
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Appointments.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Appointments.csv'
 INTO TABLE Appointments
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Lab_Results.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Lab_Results.csv'
 INTO TABLE Lab_Results
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Medications.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Medications.csv'
 INTO TABLE Medications
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Prescriptions.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Prescriptions.csv'
 INTO TABLE Prescriptions
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Specialist.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Specialist.csv'
 INTO TABLE Specialists
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Diseases.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Diseases.csv'
 INTO TABLE Diseases
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE '/path/to/Disease_Medication.csv'
+LOAD DATA LOCAL INFILE '/home/ubuntu/test-sql/Tables/Disease_Medication.csv'
 INTO TABLE Disease_Medication
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
